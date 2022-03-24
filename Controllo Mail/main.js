59050c55b8ecdd8f65cbaeb760a1e10213c99a1c
udinocera <udinocera@gmail.com>
2022-03-24 16:54:07 +0100

    Checkpoint 2

---
+++ Controllo Mail/main.js
@@ -0,0 +1,19 @@
+let email = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
+console.log(email);
+
+let inserire = prompt("Dobbiamo controllare la tua email, inseriscila");
+console.log(inserire);
+let collegare;
+
+for(let i = 0; i<email.length; i++){
+    if (inserire==email[i]){
+        alert("La tua email è accettata");
+collegare = inserire;
+    }
+
+}
+let risposta;
+if (collegare!=inserire){
+    alert("la tua email non è accettata");
+}
+
