bcae5c38eadfd27fc9a1e8fa956254618a4317df
udinocera <udinocera@gmail.com>
2022-03-24 15:12:30 +0100

    Primo Check-Point

---
+++ Gioco Dei Dadi/main.js
@@ -0,0 +1,17 @@
+let personalcomputer = Math.floor( ( Math.random() * 6 ) + 1 );
+
+console.log("Il computer lancia il numero: " + personalcomputer);
+
+let giocatore1 = Math.floor( ( Math.random() * 6 ) + 1 );
+
+console.log("Hai lanciato il numero: " + giocatore1);
+
+if(giocatore1 > personalcomputer) {
+    console.log("Vincente!");
+}else if(giocatore1 < personalcomputer) {
+    console.log("Perdente!");
+}else if(giocatore1 = personalcomputer) {
+    console.log("Pareggio!");
+}
+    
+
